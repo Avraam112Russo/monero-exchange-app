563520f14c5b4e0ca271348936fcaf11a998f29d
Russo Avram <avraam112russo@gmail.com>
2024-06-18 18:08:26 +0300

    сщььше

--- src/main/java/com/n1nt3nd0/moneroexchangeapp/config/BotConfig.java
+++ src/main/java/com/n1nt3nd0/moneroexchangeapp/config/BotConfig.java
@@ -1,17 +1,14 @@
 package com.n1nt3nd0.moneroexchangeapp.config;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 @Configuration
-@PropertySource("classpath:application.yaml")
-@Data
 public class BotConfig {
-    @Value("${bot.name}")
-    private String botName;
-    @Value("${bot.key}")
-    private String botKey;
-
+    @Bean
+    public TelegramClient telegramClient(){
+        return new OkHttpTelegramClient("7438604237:AAH-rHbOYV5xQ1eamxwSKa9XdUuRM_hysAs");
+    }
 }
